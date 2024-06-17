e3faf31b5a459c60d5039ae49398672393ee4455
Ravo1401 <hei.ravo@gmail.com>
2024-06-17 14:19:56 +0200

    initial commit

---
+++ src/main/java/mpandeha/Carte.java
@@ -0,0 +1,4 @@
+package mpandeha;
+
+public class Carte {
+}
---
+++ src/test/java/mpandeha/MarcheurTest.java
@@ -0,0 +1,10 @@
+package mpandeha;
+
+import org.junit.jupiter.api.Test;
+
+public class MarcheurTest {
+    @Test
+    public void testMpandeha() {
+
+    }
+}
